f09f45a3c4d664a0bd7e1df275945647fecb61f3
affandzakyafif <affandzaky@gmail.com>
2019-10-26 11:06:00 +0700

    evaluasi_minggu_ke3

---
+++ evaluasi mingggu ke 3.2.js
@@ -0,0 +1,15 @@
+
+const function_ke_1 = (func1) => {
+    return `ini function ke ${func1}!`;
+
+};
+document.write(function_ke_1("4"))
+
+document.write("<br>")
+
+
+const biodata = (nama, usia) => {
+    return `nama saya ${nama}! usia ${usia} tahun`;
+}
+document.write(biodata('affan', '18'))
+
---
+++ evaluasi minggu k 3.js
@@ -0,0 +1,46 @@
+
+const function_ke_1 = (func1) => {
+    return 'ini function ke,'+ func1 + '!'
+};
+console.log(function_ke_1(4))
+
+document.write("<br>")
+
+const biodata = (nama,usia) => {
+    return 'nama saya' + nama + ' usia saya' + usia + 'tahun'
+}
+console.log(biodata('affan','18'));
+
+
+
+
+document.write("<br>")
+
+
+
+
+
+
+
+const function_ke_1 = (func1) => {
+    return `ini function ke ${func1}!`;
+
+};
+document.write(function_ke_1("5"))
+
+document.write("<br>")
+
+
+const biodata = (nama, usia) => {
+    return `nama saya ${nama}! usia ${usia}`;
+}
+document.write(biodata('affan', '18'))
+
+
+
+
+
+
+
+
+
